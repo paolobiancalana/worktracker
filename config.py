bee6cbd27378e3714682a357e40723be6d0e2288
paolobiancalana <biancalana.paolo@gmail.com>
2024-08-05 19:02:16 +0200

    - Added WORK_START_TIME and WORK_END_TIME to configure the standard workday hours. - Added LUNCH_START_TIME and LUNCH_END_TIME for configurable lunch break timings. - Added BREAK_DURATION to define the standard duration for short breaks. - Introduced IDLE_BUFFER_TIME to add a buffer time before considering an idle status change. - Added DISCORD_IDLE_TIME to handle Discord's automatic idle status change. - Parameters can now be fine-tuned for flexible work tracking configurations.

--- config.py
+++ config.py
@@ -6,12 +6,24 @@ load_dotenv()
 class Config:
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
     GUILD_ID = os.getenv('GUILD_ID')
-    WORK_START_TIME = "08:00"
-    WORK_END_TIME = "19:00"
-    LUNCH_START_TIME = "12:45"
-    LUNCH_END_TIME = "14:30"
-    BREAK_DURATION = 15  # in minutes
-    DISCORD_IDLE_TIME = 5  # in minutes
+    
+    # Orari di lavoro
+    WORK_START_TIME = "07:00"  # Orario di inizio della giornata lavorativa
+    WORK_END_TIME = "18:00"  # Orario di fine della giornata lavorativa
+    
+    # Orari della pausa pranzo
+    LUNCH_START_TIME = "12:45"  # Orario di inizio della pausa pranzo
+    LUNCH_END_TIME = "14:30"  # Orario di fine della pausa pranzo
+    
+    # Durata delle pause
+    BREAK_DURATION = 15  # Durata massima di una pausa breve (in minuti)
+    
+    # Buffer per lo stato IDLE
+    IDLE_BUFFER_TIME = 5  # Tempo aggiuntivo da attendere quando l'utente passa a IDLE (in minuti)
+    
+    # Altre configurazioni
+    DISCORD_IDLE_TIME = 10  # Tempo dopo il quale Discord cambia lo stato in idle (in minuti)
+    
     TESTING = os.getenv('TESTING', 'False').lower() == 'true'
     TEST_USER_DISCORD_ID = os.getenv('TEST_USER_DISCORD_ID')
     SIMULATE_WORK_HOURS = os.getenv('SIMULATE_WORK_HOURS', 'False').lower() == 'true'
